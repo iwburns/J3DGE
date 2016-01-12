d4ceb5144447bce2e582f3955409e0146b634078
Ian Burns <iwburns8@gmail.com>
2016-01-11 23:25:30 -0600

    Separate Rendering/Updating and event polling into two different threads

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -29,6 +29,9 @@ public class Engine {
 
     public Game game;
 
+    Object lock = new Object();
+    boolean destroyed = false;
+
     public Engine(Game g) {
         errorCallback = GLFWErrorCallback.createPrint(System.err);
         game = g;
@@ -48,18 +51,7 @@ public class Engine {
 
     private void init() {
         initGLFW();
-        initOpenGL();
-        game.init();
         timer.init();
-
-        //This has been moved after game.init() because the game needs to be able to define its keyboard
-        keyboard = game.getKeyboard();
-        mouse = game.getMouse();
-
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window.getWindowHandle(), keyboard.getGLFWKeyCallback());
-        glfwSetCursorPosCallback(window.getWindowHandle(), mouse.getCursorPosCallback());
-        glfwSetMouseButtonCallback(window.getWindowHandle(), mouse.getMouseButtonCallback());
     }
 
     private void initGLFW() {
@@ -86,69 +78,25 @@ public class Engine {
                 (vidmode.width() - game.getWidth()) / 2,
                 (vidmode.height() - game.getHeight()) / 2
         );
-
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window.getWindowHandle());
-        // Enable v-sync
-        glfwSwapInterval(1);
-
-        // Make the window visible
-        glfwShowWindow(window.getWindowHandle());
-    }
-
-    private void initOpenGL() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
-        GL.createCapabilities();
-        //all initialization and gl code needs to be after the above call.
-
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_CULL_FACE);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_PROGRAM_POINT_SIZE);
-    }
-
-    private void update(float delta) {
-        game.update(delta);
-        if (game.shouldClose()) {
-            window.setShouldClose();
-        }
-    }
-
-    private void render() {
-        renderer.render(game.getScene(), game.getCamera());
-        window.update(game.getCamera());
     }
 
     private void loop() {
 
-        float delta;
+        Thread renderThread = new Thread(new RenderClass());
+
+        renderThread.start();
 
         while (!window.shouldClose()) {
-            delta = timer.getDelta();
-            glfwPollEvents();
-
-            update(delta);
-            mouse.resetDeltas();
-            timer.updateUPS();
-
-            render();
-            timer.updateFPS();
-
-            timer.update();
-            //TODO: add some kind of sync method similar to this: https://github.com/SilverTiger/lwjgl3-tutorial/blob/master/src/silvertiger/tutorial/lwjgl/core/Game.java
+            glfwWaitEvents();
         }
     }
 
     private void cleanUpWindow() {
         // Release window and window callbacks
-        window.destroy();
+        synchronized (lock) {
+            destroyed = true;
+            window.destroy();
+        }
         keyboard.destroy();
         mouse.destroy();
     }
@@ -158,4 +106,82 @@ public class Engine {
         glfwTerminate();
         errorCallback.release();
     }
+
+    //TODO: come up with a better name for this and possibly put it somewhere else.
+    private class RenderClass implements Runnable {
+
+        private void initOpenGL() {
+            glfwMakeContextCurrent(window.getWindowHandle());
+
+            // Enable v-sync
+            glfwSwapInterval(1);
+
+            // Make the window visible
+            glfwShowWindow(window.getWindowHandle());
+
+            // This line is critical for LWJGL's interoperation with GLFW's
+            // OpenGL context, or any context that is managed externally.
+            // LWJGL detects the context that is current in the current thread,
+            // creates the GLCapabilities instance and makes the OpenGL
+            // bindings available for use.
+            GL.createCapabilities();
+            //all initialization and gl code needs to be after the above call.
+
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+            glEnable(GL_DEPTH_TEST);
+            glEnable(GL_CULL_FACE);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_PROGRAM_POINT_SIZE);
+
+            game.init();
+            //This has been moved after game.init() because the game needs to be able to define its keyboard
+            keyboard = game.getKeyboard();
+            mouse = game.getMouse();
+
+            // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+            glfwSetKeyCallback(window.getWindowHandle(), keyboard.getGLFWKeyCallback());
+            glfwSetCursorPosCallback(window.getWindowHandle(), mouse.getCursorPosCallback());
+            glfwSetMouseButtonCallback(window.getWindowHandle(), mouse.getMouseButtonCallback());
+        }
+
+        private void update(float delta) {
+            game.update(delta);
+            if (game.shouldClose()) {
+                window.setShouldClose();
+            }
+        }
+
+        private void render() {
+            renderer.render(game.getScene(), game.getCamera());
+            window.update(game.getCamera());
+        }
+
+        @Override
+        public void run() {
+
+            initOpenGL();
+
+            float delta;
+
+            while (!destroyed) {
+                delta = timer.getDelta();
+
+                update(delta);
+                mouse.resetDeltas();
+                timer.updateUPS();
+
+                render();
+                timer.updateFPS();
+
+                timer.update();
+                //TODO: add some kind of sync method similar to this: https://github.com/SilverTiger/lwjgl3-tutorial/blob/master/src/silvertiger/tutorial/lwjgl/core/Game.java
+            }
+        }
+
+
+
+    }
+
 }
